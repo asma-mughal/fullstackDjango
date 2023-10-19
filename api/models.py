ea107befffe243bd3614fd18961fb0f8acfb1d65
unknown <asmamughal097@gmail.com>
2023-10-19 19:47:03 +0500

    another task

--- api/models.py
+++ api/models.py
@@ -6,11 +6,15 @@ import random, string
 # 2. Serializer 
 # 3. Views
 def generate_unique_code():
-    length =  6
+    length = 6
+
     while True:
-        code = '.'.join(random.choices(string.ascii_uppercase, k =length))
+        code = ''.join(random.choices(string.ascii_uppercase, k=length))
         if Room.objects.filter(code=code).count() == 0:
             break
+
+    return code
+
 class Room(models.Model):
     code = models.CharField(max_length=8, default=generate_unique_code, unique=True)
     host = models.CharField(max_length=50, unique=True)
